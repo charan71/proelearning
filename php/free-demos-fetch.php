582bb9b6a4ec56ede1c191a1dc77360198aa8d60
charan71 <sivacharan.p@prodesigntek.com>
2019-02-25 18:55:43 +0530

    Changed the display order of free demos. Displays latest first.

--- php/free-demos-fetch.php
+++ php/free-demos-fetch.php
@@ -3,7 +3,7 @@
 include("../php/config.php");
 
 $conn = new mysqli($HOSTNAME, $USERNAME, $PASSWORD, $DATABASE);
-$query = $conn->query("SELECT * FROM `free_demos_tbl` ORDER BY `course_name` ASC");
+$query = $conn->query("SELECT * FROM `free_demos_tbl` ORDER BY `id` DESC");
 $ar = array();
 
 while($row = mysqli_fetch_assoc($query)) {
